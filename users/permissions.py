e9819bf4a4a6d182cbfa92cf9077fc8e7616d277
Cooper <dancersoftaranto@gmail.com>
2023-01-13 15:44:06 +0300

    comments added

--- users/permissions.py
+++ users/permissions.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 
 class IsOwnerOrStaff(permissions.BasePermission):
   
+  #? 2 tane permission metodu var, obj seviyesi olanı override ediyoruz.
   # def has_permission(self, request, view):                --> view seviyesi
   # def has_object_permission(self, request, view, obj):    --> object seviyesi
   
--- users/serializers.py
+++ users/serializers.py
@@ -10,8 +10,10 @@ from .models import Profile
 class RegisterSerializer(serializers.ModelSerializer):
   email = serializers.EmailField(required=True, 
                                  validators=[UniqueValidator(queryset=User.objects.all())])
+  
   password = serializers.CharField(write_only=True, required=True,
                                    validators=[validate_password], style={"input_type" : "password"} ) 
+  
   password2 = serializers.CharField(write_only=True, required=True, style={"input_type" : "password"}) 
   
   class Meta:
@@ -60,9 +62,10 @@ class ProfileSerializer(serializers.ModelSerializer):
   user_id = serializers.IntegerField(required=False)
   class Meta:
     model = Profile
-    fields = "__all__"
+    fields = ("id","user","user_id", "display_name","avatar", "bio")
     
-
+#! buradaki ProfileSerializer'ın kullanıldığı view (ProfileUpdateView)
+#! RetrieveUpdateAPIView'dan inherit edildiği için create metodu değil update metodu override edilmeli,
   def update(self, instance, validated_data):
     instance = super().update(instance, validated_data)
     instance.user_id = self.context["request"].user.id
